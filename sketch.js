c143cf3a72cdc9127e8324128fef740689547ce9
Unknown <tylerchasenielsen@gmail.com>
2017-10-09 09:10:04 -0700

    Taught zombies how to walk
    They can also teleport, and are bigger and bolder than ever before!

--- sketch.js
+++ sketch.js
@@ -1,12 +1,32 @@
 // Zombulator by Tyler Nielsen
 
+var zombieX = 50;
+var zombie2X = 80;
+var xMax = 1000;
+var xMin = 0;
+var speed1 = 5;
+var size = 80;
+
 function setup() {
-	createCanvas(800, 800);
+  createCanvas(xMax, 1000);
 }
 
 function draw() {
-  fill(255, 0, 0);
-  ellipse(50, 50, 80, 80);
+  background(255, 255, 255);
+  fill(random(255), random(255), random(255));
+  strokeWeight(4);
+  ellipse(zombieX, 50, 80, 80);
   fill(150, 150, 200);
   ellipse(200, 100, 80, 80);
+  zombieX = zombieX + speed1;
+  
+  if(zombieX >= xMax || zombieX <= xMin) {
+    zombieX = -1 * zombieX;
+//    speed1 = speed1 + 5;
+//    size = size + 20;
+  }
+  if(zombie2X >= xMin) {
+    zombie2X = 0;
+  }
+
 }
