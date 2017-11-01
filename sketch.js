149a826a734f851023890409f809fc69a07ef637
Unknown <tylerchasenielsen@gmail.com>
2017-11-01 10:06:50 -0700

    Polymorphism mob coding
    Removed similar functions for humans and zombies and replaced them with universal initialize, draw, and move functions for the population.

--- sketch.js
+++ sketch.js
@@ -6,36 +6,43 @@ var backgroundColor;
 
 const MIN_SIZE = 5;
 const MAX_SIZE = 50;
-const NUMBER_OF_ZOMBIES = 100;
-const NUMBER_OF_HUMANS = 100;
+const POPULATION_SIZE = 500;
 
-var zombies;
-
-var humans;
+var population = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   backgroundColor = color(245, 255, 245);
-  initializeZombies();
-  initializeHumans();
+  initializePopulation();
 }
 
 function draw() {
   background(backgroundColor);
   noStroke();
-  drawZombies();
-  moveZombies();
-  drawHumans();
-  moveHumans();
+  drawPopulation();
+  movePopulation();
 }
 
+function initializePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    var humanoid_type = random(0, 100);
+    if (humanoid_type <= 50) {
+      population[i] = initializeZombie();
+    } else {
+      population[i] = initializeHuman();
+    }
+  }
+}
 
-// Zombies. Raaahh!
+function drawPopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    population[i].draw();
+  }
+}
 
-function initializeZombies() {
-  zombies = [];
-  for (var i = 0; i < NUMBER_OF_ZOMBIES; ++i) {
-    zombies[i] = initializeZombie();
+function movePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    population[i].move();
   }
 }
 
@@ -65,27 +72,6 @@ function initializeZombie() {
   };
 }
 
-function drawZombies() {
-  for (var i = 0; i < NUMBER_OF_ZOMBIES; ++i) {
-    zombies[i].draw();
-  }
-}
-
-function moveZombies() {
-  for (var i = 0; i < NUMBER_OF_ZOMBIES; ++i) {
-    zombies[i].move();
-  }
-}
-
-// Humans. Mmmm brains!
-
-function initializeHumans() {
-  humans = [];
-  for (var i = 0; i < NUMBER_OF_HUMANS; ++i) {
-    humans[i] = initializeHuman();
-  }
-}
-
 function initializeHuman(index) {
   return {
     x: random(0, windowWidth),
@@ -109,17 +95,5 @@ function initializeHuman(index) {
         this.y -= this.speed;
       }
     }
-  }
-}
-
-function drawHumans() {
-  for (var i = 0; i < NUMBER_OF_HUMANS; ++i) {
-    humans[i].draw();
-  }
-}
-
-function moveHumans() {
-  for (var i = 0; i < NUMBER_OF_HUMANS; ++i) {
-    humans[i].move();
   }
 }
