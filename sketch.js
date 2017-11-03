224de42e58191ce5d9383f7710670a77d3cb44c4
Unknown <tylerchasenielsen@gmail.com>
2017-11-03 09:53:46 -0700

    Began scaffolding collision logic
    Zombies and Humans are about to fight through a new function in draw: handleCollisions().

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-// http://tinyurl.com/cs160ex19
+// http://tinyurl.com/cs160ex20
 // Zombulator by Tyler Nielsen
-// CS 160 Exercise 19: Polymorphism
+// CS 160 Exercise 20: Collisions
 
 var backgroundColor;
 
@@ -9,6 +9,9 @@ const MAX_SIZE = 50;
 const POPULATION_SIZE = 500;
 
 var population = [];
+
+var zombieCount = 0;
+var humanCount = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -21,6 +24,21 @@ function draw() {
   noStroke();
   drawPopulation();
   movePopulation();
+  drawPopulationCounts();
+  handleCollisions();
+}
+
+function handleCollisions() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    var attacker = population [i];
+    for (var j = i + 1; j < POPULATION_SIZE; ++j) {
+      var target = population [j];
+
+      if (attacker.isTouching(target)) {
+        print("Fight!");
+      }
+    }
+  }
 }
 
 function initializePopulation() {
@@ -28,10 +46,20 @@ function initializePopulation() {
     var humanoid_type = random(0, 100);
     if (humanoid_type <= 50) {
       population[i] = initializeZombie();
+      ++zombieCount;
     } else {
       population[i] = initializeHuman();
+      ++humanCount;
     }
   }
+}
+
+function drawPopulationCounts() {
+  stroke(0);
+  textSize(72);
+  textAlign(CENTER);
+  text("Zombies: " + zombieCount, width / 2, 100);
+  text("Humans: " + humanCount, width / 2, height - 100);
 }
 
 function drawPopulation() {
@@ -68,32 +96,38 @@ function initializeZombie() {
     draw: function() {
       fill(this.color);
       ellipse(this.x, this.y, this.size, this.size);
+    },
+    isTouching: function(target) {
+
     }
   };
 }
 
-function initializeHuman(index) {
+function initializeHuman() {
   return {
     x: random(0, windowWidth),
     y: random(windowHeight - 200, windowHeight),
     speed: random(0.25, 3),
     size: random(MIN_SIZE, MAX_SIZE),
     color: color(random(50, 150), random(50, 150), random(150, 255), 150),
-    draw: function() {
-      fill(this.color);
-      ellipse(this.x, this.y, this.size, this.size);
-    },
     move: function() {
-      var direction = random(0, 100);
-      if (direction < 20) {
-        this.x += this.speed;
-      } else if (direction < 40) {
-        this.x -= this.speed;
-      } else if (direction < 60) {
-        this.y += this.speed;
-      } else {
-        this.y -= this.speed;
-      }
+        var direction = random(0, 100);
+        if (direction < 20) {
+          this.x += this.speed;
+        } else if (direction < 40) {
+          this.x -= this.speed;
+        } else if (direction < 60) {
+          this.y += this.speed;
+        } else {
+          this.y -= this.speed;
+        }
+      },
+    draw: function() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.size, this.size);
+    },
+    isTouching: function(target) {
+      
     }
-  }
+  };
 }
